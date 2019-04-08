41a0293cc83fa6ede94ee04ba0a06f2597511d18
huangdeng525 <winlj@sina.com>
2019-04-08 18:00:48 +0800

    temp

--- main.py
+++ main.py
@@ -45,20 +45,33 @@ def get_key(full_name, hash_type):
 
 
 class ActionBase:
-    def __init__(self, notice_array, label_array):
+    def __init__(self, notice_array, gui_base):
+        self.gui = gui_base
         self.notice_array = notice_array
-        self.label_array = label_array
+        self.label_array = gui_base.label_var
+        self.entrys = gui_base.para_entrys
 
     def update_notice(self):
         for notice, label in zip(self.notice_array, self.label_array):
             label.set(notice)
+        for entry in self.entrys:
+            entry.delete(0, tk.END)
 
+    def out(self, notice):
+        self.gui.windows[0][0].insert(tk.END, notice + '\n')
+        self.gui.windows[0][0].see(tk.END)
+        self.gui.windows[0][0].update()
 
+    def FlashOut(self, notice):
+        self.gui.windows[0][1].insert(tk.END, notice + '\n')
+        self.gui.windows[0][1].see(tk.END)
+        self.gui.windows[0][1].update()
+    
 class CheckSum(ActionBase):
     def __init__(self, parent):
         self._parent = parent
         notice_array = ['Need To Check File:', 'Alg(md5,sha1,sha256):', 'Need Check Sum(opt):','', '']
-        ActionBase.__init__(self, notice_array, parent.label_var)
+        ActionBase.__init__(self, notice_array, parent)
 
     def Act(self):
         check_file = self._parent.para_strings[0]
@@ -66,28 +79,26 @@ class CheckSum(ActionBase):
         to_check_sum = self._parent.para_strings[2]
         support_hash = ['md5', 'sha1', 'sha256']
         if len(check_file) == 0 or len(hash_type) == 0 or hash_type not in support_hash:
-            self._parent.update_outtext('para error:')
+            self.out('para error:')
             return
         if not os.path.exists(check_file):
-            notice = ['File Not Exist!']
-            self._parent.update_outtext(notice)
+            self.out('File Not Exist!')
             return
         hash_key = get_key(check_file, hash_type)
-        outs = []
-        outs.append(str(hash_key))
+        self.out(check_file)
+        self.out('[%s]%s' % (hash_type, str(hash_key)))
         if len(to_check_sum) > 0:
             if to_check_sum == hash_key:
-                outs.append('Check passed!')
+                self.out('Check Passed!')
             else:
-                outs.append('Check Failed!')
-        self._parent.update_outtext(outs)
+                self.out('Check Failed!')
 
 
 class CheckUnique(ActionBase):
     def __init__(self, parent):
         self._parent = parent
         notice_array = ['1st dir(H):', '2nd dir(opt):', '3rd dir(opt):','4th dir(L)(opt):', 'File to Save:']
-        ActionBase.__init__(self, notice_array, parent.label_var)
+        ActionBase.__init__(self, notice_array, parent)
 
     def Act(self):
         out_file = self._parent.para_strings[4]
@@ -97,18 +108,18 @@ class CheckUnique(ActionBase):
                 need_check_dir.append(self._parent.para_strings[x])
 
         if len(out_file) == 0 or len(need_check_dir) == 0:
-            self.out_unique_help()
+            self.out('para error!')
             return
         self.hash_key = dict()
         self.files = 0
         self.empty_files = []
         with open(out_file, 'w', encoding="utf-8") as self.out_f:
             for one in need_check_dir:
+                self.out('Process Dir:' + one)
                 self.process_unique(one)
             for one in self.empty_files:
                 self.out_f.write('#empty File:%s\n' % one)
-        notice = ['process success finished!']
-        self._parent.update_outtext(notice)
+        self.out('Process success finished!')
 
     def process_unique(self, root):
         if len(root) == 0:
@@ -118,8 +129,7 @@ class CheckUnique(ActionBase):
                 for file in files:
                     self.files += 1
                     if self.files % 1000 == 0:
-                        ptr_str = ['\r processed files:%d' % self.files]
-                        self._parent.update_outtext(ptr_str)
+                        self.out('\rProcessed files:%d' % self.files)
                     full_name = os.path.join(cur_path, file)
                     if os.path.getsize(full_name) == 0:
                         self.empty_files.append(full_name)
@@ -130,12 +140,6 @@ class CheckUnique(ActionBase):
                         self.record_equal(self.hash_key[f_key], full_name)
                     else:
                         self.hash_key[f_key] = full_name
-
-    def out_unique_help(self):
-        notice = ['Please input paramater:',
-                'para1~para4:the dir to find equal files, High priority ahead',
-                'para5:the file to out the equal files, can be run in command']
-        self._parent.update_outtext(notice)
 
     def record_equal(self, ori, equal):
         outs = ['#Ori:%s\n' % ori,
@@ -148,7 +152,7 @@ class HashDir(ActionBase):
     def __init__(self, parent):
         self._parent = parent
         notice_array = ['1st dir:', '2nd dir(opt):', '3rd dir(opt):','4th dir(opt):', 'File to Save:']
-        ActionBase.__init__(self, notice_array, parent.label_var)
+        ActionBase.__init__(self, notice_array, parent)
 
     def Act(self):
         out_file = self._parent.para_strings[4]
@@ -188,7 +192,7 @@ class Split(ActionBase):
     def __init__(self, parent):
         self._parent = parent
         notice_array = ['To Split File:', '1st Save File:', '2st Save File:','3st Save File(opt):', '4st Save File(opt):']
-        ActionBase.__init__(self, notice_array, parent.label_var)
+        ActionBase.__init__(self, notice_array, parent)
 
     def Act(self):
         input_file = self._parent.para_strings[0]
@@ -229,7 +233,7 @@ class Merge(ActionBase):
     def __init__(self, parent):
         self._parent = parent
         notice_array = ['1st Merge File:', '2st Merge File:','3st Merge File(opt):', '4st Merge File(opt):','To Save File:', ]
-        ActionBase.__init__(self, notice_array, parent.label_var)
+        ActionBase.__init__(self, notice_array, parent)
 
     def Act(self):
         outs_file = self._parent.para_strings[4]
@@ -274,16 +278,20 @@ class GUI:
         self.windows = []
         self.para_entrys = []
         self.label_var = []
-
-        my_font = ('Lucida Console', 12)
+        self.my_font = ('Lucida Console', 12)
+        self.my_color = ['#C7EDCC', 'black']
 
         self.frame.append(tk.Frame(self.root, borderwidth=0)) 
-        outText = tk.Text(self.frame[0], height=26, width=120, font=my_font, bg='#C7EDCC', fg='black', relief=tk.FLAT)
+        outText = tk.Text(self.frame[0], height=20, width=120, font=self.my_font, bg='#C7EDCC', fg='black', relief=tk.FLAT)
         scroll = tk.Scrollbar(self.frame[0], command=outText.yview)
         outText.configure(yscrollcommand=scroll.set)
-        outText.pack(side=tk.LEFT)
+        outText.pack(side=tk.TOP)
         scroll.pack(side=tk.RIGHT, fill=tk.Y)
-        self.windows.append([outText])
+
+        FlashText = tk.Text(self.frame[0], height=6, width=120, font=self.my_font, bg='#C7EDCC', fg='black', relief=tk.FLAT)
+        FlashText.pack(side=tk.BOTTOM)
+
+        self.windows.append([outText, FlashText])
         self.frame[0].pack()
 
         col = 0
@@ -291,18 +299,17 @@ class GUI:
             col += 1
             label_var = tk.StringVar()
             self.frame.append(tk.Frame(self.root, borderwidth=0))
-            in_label = tk.Label(self.frame[col], width=30, font=my_font, textvariable=label_var)
+            in_label = tk.Label(self.frame[col], width=30, font=self.my_font, textvariable=label_var)
             self.label_var.append(label_var)
             in_label.pack(side=tk.LEFT)
             e = tk.StringVar()
-            in_entry = tk.Entry(self.frame[col], width=90, font=my_font, textvariable=e)
+            in_entry = tk.Entry(self.frame[col], width=90, font=self.my_font, textvariable=e)
             in_entry.pack(side=tk.LEFT)
             e.set("")
             self.windows.append([in_label, in_entry])
             self.para_entrys.append(in_entry)
             self.frame[col].pack()
 
-        
         self.frame.append(tk.Frame(self.root, borderwidth=0))
         self.SelectedFun = tk.IntVar()
         for text, value in [('checksum', 1), ('unique', 2), ('split', 3), ('merge', 4), ('hashdir', 5)]:
@@ -319,6 +326,22 @@ class GUI:
         self.frame[-1].pack()
 
         self.actions = [CheckSum(self), CheckUnique(self), Split(self), Merge(self), HashDir(self), ]
+
+    def crt_outtext(self):
+                self.out_text_frame = tk.Frame(self.root, borderwidth=1)
+        scroll_y = tk.Scrollbar(self.out_text_frame)
+        scroll_y.pack(side=tk.RIGHT, fill=tk.Y)
+        scroll_x = tk.Scrollbar(self.out_text_frame, orient=tk.HORIZONTAL)
+        scroll_x.pack(side=tk.BOTTOM, fill=tk.X)
+
+        self.out_text = tk.Text(self.out_text_frame,height=22, width=120, font=self.my_font, bg='#C7EDCC', fg='black', \
+                            yscrollcommand=scroll_y.set, xscrollcommand=scroll_x.set, wrap='none', relief=tk.FLAT)
+        self.out_text.pack(side=tk.LEFT)
+        scroll_y.config(command=self.out_text.yview)
+        scroll_x.config(command=self.out_text.xview)
+
+        self.out_text_frame.pack(padx=1, pady=2)
+        self.out_text.config(state='disable')
 
     def SelectedAction(self, para):
         if para.get() > 0:
